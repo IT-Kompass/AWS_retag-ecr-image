937a4487d0b7fa4bed843d5cd074678d53f94d63
Kai <kai@kaiherzig.com>
2021-07-15 13:26:53 +0200

    Removed Comments to copy/paste

--- ReTag.sh
+++ ReTag.sh
@@ -1,15 +1,14 @@
-AWSProfile="<<FillMe>>" #AWS CLI Profile
-IsOldDigit=true         #If old image a Digest set to true, if the old container Value is a Tag set to false.
-OldImage="<<FILLME>>"   # IMG Digest or Tag, depending on $IsOldDigit
-NewTag="<<FILLME>>"     #What should the new tag be?
-Repository="<FILLME>>"  # Repository Name in ECR
+AWSProfile="<<FillMe>>"
+IsOldDigest=true
+OldImage="sha256:<<FillMe>>"
+NewTag="<<FillMe>>"
+Repository="<<FillMe>>"
 
-#Let's do it :)
 
-if [ $IsOldDigit = true ]; then
+if [ $IsOldDigest = true ]; then
     MANIFEST=$(aws ecr batch-get-image --repository-name $Repository --image-ids imageDigest=$OldImage --query 'images[].imageManifest' --output text --profile $AWSProfile)
 fi
-if [ $IsOldDigit = false ]; then
+if [ $IsOldDigest = false ]; then
     MANIFEST=$(aws ecr batch-get-image --repository-name $Repository --image-ids imageTrue=$OldImage --query 'images[].imageManifest' --output text --profile $AWSProfile)
 fi
 
